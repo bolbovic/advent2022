3c1b3252bfb0f7418ae789200e2898809e0f0e36
Hugues <hugues.alex@gmail.com>
2022-12-25 12:57:16 +0100

    day10

--- src/server/trpc/solvers/day10.ts
+++ src/server/trpc/solvers/day10.ts
@@ -1,6 +1,21 @@
 const day10Solver = (input: string) => {
   const lines = input.split("\n");
-  return lines.length;
+  const cycles: number[] = [];
+  lines.forEach((line) => {
+    cycles.push(0);
+    if (line !== "noop") {
+      cycles.push(parseInt(line.split(" ")[1] as string));
+    }
+  });
+  let X = 1;
+  let result = 0;
+  cycles.forEach((cycle, idx) => {
+    if ([20, 60, 100, 140, 180, 220].includes(idx + 1)) {
+      result += X * (idx + 1);
+    }
+    X += cycle;
+  });
+  return result;
 };
 
 export default day10Solver;
--- src/server/trpc/solvers/day10b.ts
+++ src/server/trpc/solvers/day10b.ts
@@ -1,6 +1,27 @@
-const day10bSolver = (input: string) => {
+const day10Solver = (input: string) => {
   const lines = input.split("\n");
-  return lines.length;
+  const cycles: number[] = [];
+  lines.forEach((line) => {
+    cycles.push(0);
+    if (line !== "noop") {
+      cycles.push(parseInt(line.split(" ")[1] as string));
+    }
+  });
+  let X = 1;
+  let render = "";
+  cycles.forEach((cycle, idx) => {
+    if ([40, 80, 120, 160, 200].includes(idx)) {
+      render += "\n";
+    }
+    if (Math.abs(X - (idx % 40)) > 1) {
+      render += ".";
+    } else {
+      render += "#";
+    }
+    X += cycle;
+  });
+  console.log(render);
+  return render;
 };
 
-export default day10bSolver;
+export default day10Solver;
